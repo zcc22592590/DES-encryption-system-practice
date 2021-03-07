74e4bb8777ec16e6686c25e59efd6698e507be49
zcc22592590 <75067139+zcc22592590@users.noreply.github.com>
2021-03-07 21:42:57 +0800

    version4
    1. 新增忽略「scanf返回值警告」

--- DES-encryption-system(ex1).c
+++ DES-encryption-system(ex1).c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	
 	int SBOX[4][16] =  {{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
@@ -8,13 +8,13 @@ int main()
 						{4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
 						{15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}};
 	
-	char input[6];
+	char input[5];
 	
 	for(;;){
 	
 	
 	printf("please input a 6-digit number(bi), or enter \"bye\" to end: ");
-	scanf("%s", &input);
+	if(scanf("%s", &input)){};
 
 	
 	int bk0 = input[0]; 
@@ -48,4 +48,5 @@ int main()
 	}
 	printf("\n");
 	}
+	return 0;
  } 
