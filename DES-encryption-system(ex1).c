9892543f1fa8feb87ea264f36d7e5e4e774010c9
zcc22592590 <75067139+zcc22592590@users.noreply.github.com>
2021-03-09 17:36:18 +0800

    final version

--- DES-encryption-system(ex1).c
+++ DES-encryption-system(ex1).c
@@ -1,52 +1,27 @@
-#include <stdio.h>
-
-int main(void)
+#include<stdio.h>
+#include<stdlib.h>
+int main()
 {
+	int sbox[4][16]={{14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},{0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8},{4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},{15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13}};
 	
-	int SBOX[4][16] =  {{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
-						{0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
-						{4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
-						{15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}};
+	char sb[7];
+	int row,col;
+	int x1,x2,a,b,c,d;
 	
-	char input[5];
-	
-	for(;;){
-	
-	
-	printf("please input a 6-digit number(bi), or enter \"bye\" to end: ");
-	if(scanf("%s", &input)){};
-
-	
-	int bk0 = input[0]; 
-	int bk1 = input[1];
-	int bk2 = input[2];
-	
-	if(bk0 == 98 & bk1 == 121 & bk2 == 101){ 
-		break;
+	while(scanf("%s",sb)!=EOF)
+	{
+	row=(sb[0]-48)*2+(sb[5]-48)*1;
+	col=(sb[1]-48)*8+(sb[2]-48)*4+(sb[3]-48)*2+(sb[4]-48)*1;
+	x1=sbox[row][col];
+    
+        a = x1 % 2;         
+        b = (x1 / 2)%2;  
+        c = (x1 / 4)%2;    
+        d = (x1 / 8)%2;      
+        printf("%d%d%d%d",d,c,b,a);
+    
+    printf("\n");
 	}
 	
-	
-	int r = (input[0] - '0')*2 + (input[5] - '0');
-	int c = (input[1] - '0')*8 + (input[2] - '0')*4 + (input[3] - '0')*2 + (input[4] - '0');
-	
-	
-	int n = SBOX[r][c];
-	int a[10];
-	int i = 0;
-	
-	
-	
-	while(n>0){
-		a[i] = n%2;
-		i = i+1;
-		n = n/2;
-	}
-	
-	
-	for(; i > 0; i--){
-		printf("%d", a[i-1]);
-	}
-	printf("\n");
-	}
-	return 0;
- } 
+    return 0;
+}
